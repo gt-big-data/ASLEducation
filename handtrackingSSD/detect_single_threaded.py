dddf81dda936a2a00416cd2ab298eed70869d0cf
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-10-26 00:56:53 -0400

    generated cropped images

--- handtrackingSSD/detect_single_threaded.py
+++ handtrackingSSD/detect_single_threaded.py
@@ -4,7 +4,16 @@ import tensorflow as tf
 import datetime
 import argparse
 
+import dataset_gen
+
 detection_graph, sess = detector_utils.load_inference_graph()
+
+path_to_train_images = 'Image-Annotation-5/train/images'
+# path_to_train_labels = '/content/Image-Annotation-5/train/labels'
+path_to_test_images = 'Image-Annotation-5/test/images'
+# path_to_test_labels = '/content/Image-Annotation-5/test/labels'
+path_to_val_images = 'Image-Annotation-5/valid/images'
+# path_to_val_labels = '/content/Image-Annotation-5/valid/labels'
 
 if __name__ == '__main__':
 
@@ -16,71 +25,67 @@ if __name__ == '__main__':
         type=float,
         default=0.2,
         help='Score threshold for displaying bounding boxes')
-    parser.add_argument(
-        '-fps',
-        '--fps',
-        dest='fps',
-        type=int,
-        default=1,
-        help='Show FPS on detection/display visualization')
-    parser.add_argument(
-        '-src',
-        '--source',
-        dest='video_source',
-        default=0,
-        help='Device index of the camera.')
-    parser.add_argument(
-        '-wd',
-        '--width',
-        dest='width',
-        type=int,
-        default=320,
-        help='Width of the frames in the video stream.')
-    parser.add_argument(
-        '-ht',
-        '--height',
-        dest='height',
-        type=int,
-        default=180,
-        help='Height of the frames in the video stream.')
-    parser.add_argument(
-        '-ds',
-        '--display',
-        dest='display',
-        type=int,
-        default=1,
-        help='Display the detected images using OpenCV. This reduces FPS')
-    parser.add_argument(
-        '-num-w',
-        '--num-workers',
-        dest='num_workers',
-        type=int,
-        default=4,
-        help='Number of workers.')
-    parser.add_argument(
-        '-q-size',
-        '--queue-size',
-        dest='queue_size',
-        type=int,
-        default=5,
-        help='Size of the queue.')
+    # parser.add_argument(
+    #     '-fps',
+    #     '--fps',
+    #     dest='fps',
+    #     type=int,
+    #     default=1,
+    #     help='Show FPS on detection/display visualization')
+    # parser.add_argument(
+    #     '-src',
+    #     '--source',
+    #     dest='video_source',
+    #     default=0,
+    #     help='Device index of the camera.')
+    # parser.add_argument(
+    #     '-wd',
+    #     '--width',
+    #     dest='width',
+    #     type=int,
+    #     default=320,
+    #     help='Width of the frames in the video stream.')
+    # parser.add_argument(
+    #     '-ht',
+    #     '--height',
+    #     dest='height',
+    #     type=int,
+    #     default=180,
+    #     help='Height of the frames in the video stream.')
+    # parser.add_argument(
+    #     '-ds',
+    #     '--display',
+    #     dest='display',
+    #     type=int,
+    #     default=1,
+    #     help='Display the detected images using OpenCV. This reduces FPS')
+    # parser.add_argument(
+    #     '-num-w',
+    #     '--num-workers',
+    #     dest='num_workers',
+    #     type=int,
+    #     default=4,
+    #     help='Number of workers.')
+    # parser.add_argument(
+    #     '-q-size',
+    #     '--queue-size',
+    #     dest='queue_size',
+    #     type=int,
+    #     default=5,
+    #     help='Size of the queue.')
     args = parser.parse_args()
-
-    cap = cv2.VideoCapture(args.video_source)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.height)
-
+    
     start_time = datetime.datetime.now()
     num_frames = 0
-    im_width, im_height = (cap.get(3), cap.get(4))
+    im_width, im_height = (512, 513)
     # max number of hands we want to detect/track
-    num_hands_detect = 2
+    num_hands_detect = 1
 
     cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
 
-    while True:
+    for image_path in dataset_gen.generate_stream(path_to_val_images):
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        ret, image_np = cap.read()
+        image_np = cv2.imread(image_path)
         # image_np = cv2.flip(image_np, 1)
         try:
             image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
@@ -93,29 +98,30 @@ if __name__ == '__main__':
 
         boxes, scores = detector_utils.detect_objects(image_np,
                                                       detection_graph, sess)
-
-        # draw bounding boxes on frame
-        detector_utils.draw_box_on_image(num_hands_detect, args.score_thresh,
+        
+        # # draw bounding boxes on frame
+        # detector_utils.draw_box_on_image(num_hands_detect, args.score_thresh,
+        #                                  scores, boxes, im_width, im_height,
+        #                                  image_np, 0.75)
+        image_display = detector_utils.get_image_cropped(num_hands_detect, args.score_thresh,
                                          scores, boxes, im_width, im_height,
-                                         image_np)
-
+                                         image_np, 0.75)
+        
         # Calculate Frames per second (FPS)
         num_frames += 1
         elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
         fps = num_frames / elapsed_time
 
-        if (args.display > 0):
-            # Display FPS on frame
-            if (args.fps > 0):
-                detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
-                                                 image_np)
-
+        if (len(boxes) >= 1):
+            # cv2.drawContours(image_np, contours=(77, 255, 9))
             cv2.imshow('Single-Threaded Detection',
-                       cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
+                       cv2.cvtColor(image_display, cv2.COLOR_RGB2BGR))
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
+            
+            dataset_gen.saveImage(image_display, 'Image-Cropped/val/images', image_path.split('/')[-1])
         else:
             print("frames processed: ", num_frames, "elapsed time: ",
                   elapsed_time, "fps: ", str(int(fps)))
