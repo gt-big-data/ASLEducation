70ad31d786a264331fb8c3a8eb065069c262390e
Anthony Nguyen <100966897+anthonyvnguyen@users.noreply.github.com>
2023-10-27 18:30:30 -0400

    Added 3 pages to work on

--- DATAVIZ/asl-app/src/App.js
+++ DATAVIZ/asl-app/src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//Inmporting the pages and components
+import Navbar from './components/navBar.js';
+import Home from './pages/homePage.js';
+import OurTeam from './pages/ourTeamPage.js';
+import AboutOurModel from './pages/aboutOurModel.js';
+
 function App() {
   return (
     <div className="App">
