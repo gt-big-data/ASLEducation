8c93da1bf2094f77131f518f9174a794587ffc85
joshkabloomy <joshkabloomy1220@gmail.com>
2023-11-09 18:39:36 -0500

    init

---
+++ API_AWS/ASL_Hand_Classifier.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+
+model = load_model('resnet101_seg.h5')
+labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
+          7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger',
+          14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
+          19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple',
+          26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn',
+          32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
+
+
+def processed_img(img_path):
+    img = load_img(img_path, target_size=(224, 224, 3))
+    img = img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, [0])
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    print(res)
+    return res.capitalize()
+
+
+def run():
+    st.title("ASL Hand Classification")
+    img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+    if img_file is not None:
+        img = Image.open(img_file).resize((250, 250))
+        st.image(img, use_column_width=False)
+        save_image_path = './upload_images/' + img_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
+
+        # if st.button("Predict"):
+        if img_file is not None:
+            result = processed_img(save_image_path)
+            print(result)
+
+run()
---
+++ API_AWS/ec2_api.py
@@ -0,0 +1,47 @@
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from flask import Flask, jsonify, request
+
+
+model = load_model('resnet101_seg.h5')
+
+labels = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
+12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R',18: 'S',19: 'T',20: 'U',21: 'V',22: 'W',23: 'X',24: 'Y',25: 'Z'}
+
+
+def prepare_image(img_path):
+    img = load_img(img_path, target_size=(224, 224, 3))
+    img = img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, [0])
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    print(res)
+    return res
+    #return res.capitalize()
+
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def infer_image():
+    if 'file' not in request.files:
+        return jsonify(error="Please try again. The Image doesn't exist")
+
+    file = request.files.get('file')
+    img_bytes = file.read()
+    img_path = "./upload_images/test.jpg"
+    with open(img_path, "wb") as img:
+        img.write(img_bytes)
+    result = prepare_image(img_path)
+    return jsonify(prediction=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
