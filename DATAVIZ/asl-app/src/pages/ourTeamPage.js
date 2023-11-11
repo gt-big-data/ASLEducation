b4e0b0e0a6d7ff75d752b0ad712b696e2799d920
reedashimaz <133261457+reedashimaz@users.noreply.github.com>
2023-11-11 03:31:26 +0300

    Update ourTeamPage.js

--- DATAVIZ/asl-app/src/pages/ourTeamPage.js
+++ DATAVIZ/asl-app/src/pages/ourTeamPage.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
-const OutTeam = () => {
-  
-      return (
-      <ChakraProvider name="ourTeam" >
+import React from 'react';
+import { ChakraProvider, Heading, Box, Text, Flex } from '@chakra-ui/react';
 
-      </ChakraProvider>
-      )
-  };
-      
-  export default OurTeam;
+const teamMembers = [
+    { name: 'Advay Balakrishnan', role: 'Analysis'},
+    { name: 'Joshua Diao', role: 'Analysis'},
+    { name: 'Erel Ozen', role: 'Data Viz'},
+    { name: 'Pranav Somu', role: 'Analysis'}
+];
+
+const TeamMember = ({ name, role }) => (
+    <Box p={4} shadow="md" borderWidth="1px" borderRadius="md">
+        <Heading as="h3" size="md">{name}</Heading>
+        <Text mt={2} color="blue.300">{role}</Text>
+    </Box>
+)
+
+const OurTeam = () => {
+    console.log("OurTeam component rendered!"); // Check if component is rendered
+    return (
+        <ChakraProvider>
+            <Box p={8}>
+                <Heading size="xl" mb={4}>Meet the Team</Heading>
+                <Flex flexWrap="wrap">
+                    {teamMembers.map((member, index) => (
+                        <TeamMember key={index} name={member.name} role={member.role} />
+                    ))}
+                </Flex>
+            </Box>
+        </ChakraProvider>
+    )
+};
+
+export default OurTeam;
