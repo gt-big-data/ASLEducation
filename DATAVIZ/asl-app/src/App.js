4b58a587e1c74ed9e5b0febe6dadd2cefc2e1ee7
reedashimaz <133261457+reedashimaz@users.noreply.github.com>
2023-11-11 03:32:07 +0300

    Update App.js

--- DATAVIZ/asl-app/src/App.js
+++ DATAVIZ/asl-app/src/App.js
@@ -1,8 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-
-
-//Inmporting the pages and components
+import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react'
 import Navbar from './components/navBar.js';
 import Home from './pages/homePage.js';
@@ -12,14 +8,12 @@ import AboutOurModel from './pages/aboutOurModel.js';
 function App() {
   return (
     <ChakraProvider>
-
-    <div className="App">
-      <Navbar />
-      <Home />
-      <OurTeam />
-      <AboutOurModel />
-    </div>
-
+      <div className="App">
+        <Navbar />
+        <Home />
+        <OurTeam />
+        <AboutOurModel />
+        </div>
     </ChakraProvider>
   );
 }
